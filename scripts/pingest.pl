a448b4ac72dc884466e6265e2e624cbb70b1b497
Jason Stephenson <jstephenson@mvlc.org>
2014-01-06 14:50:09 -0500

    Remove HTML from pingest.pl.
    Looks like saving the file from a gist on github was a bad idea.
    
    Thanks to Chris Sharp of Georgia Pines for pointing this out.
    
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- scripts/pingest.pl
+++ scripts/pingest.pl
@@ -1,9 +1,6 @@
-
-<!-- saved from url=(0095)https://gist.github.com/Dyrcona/7807082/raw/704cfb28a458b3e6e7b1389f35f678fe48b4b0b3/pingest.pl -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">#!/usr/bin/perl
 # ---------------------------------------------------------------
 # Copyright © 2013 Merrimack Valley Library Consortium
-# Jason Stephenson &lt;jstephenson@mvlc.org&gt;
+# Jason Stephenson <jstephenson@mvlc.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,16 +23,16 @@ use DBI;
 # database server.  Using roughly number of cores/2 doesn't seem to
 # have much impact in off peak times.
 use constant {
-    BATCHSIZE =&gt; 10000,
-    MAXCHILD =&gt; 8
+    BATCHSIZE => 10000,
+    MAXCHILD => 8
 };
 
 # "Gimme the keys!  I'll drive!"
-my $q = &lt;&lt;END_OF_Q;
+my $q = <<END_OF_Q;
 SELECT id
 FROM biblio.record_entry
 WHERE deleted = 'f'
-AND id &gt; 0
+AND id > 0
 ORDER BY id ASC
 END_OF_Q
 
@@ -47,20 +44,20 @@ my @lol = ();
 # To do the browse-only ingest:
 my @blist = ();
 
-# All of the DBI-&gt;connect() calls in this file assume that you have
+# All of the DBI->connect() calls in this file assume that you have
 # configured the PGHOST, PGPORT, PGDATABASE, PGUSER, and PGPASSWORD
 # variables in your execution environment.  If you have not, you have
 # two options:
 #
 # 1) configure them
 #
-# 2) edit the DBI-&gt;connec() calls in this program so that it can
+# 2) edit the DBI->connec() calls in this program so that it can
 # connect to your database.
-my $dbh = DBI-&gt;connect('DBI:Pg:');
+my $dbh = DBI->connect('DBI:Pg:');
 
-my $results = $dbh-&gt;selectall_arrayref($q);
+my $results = $dbh->selectall_arrayref($q);
 foreach my $r (@$results) {
-    my $record = $r-&gt;[0];
+    my $record = $r->[0];
     push(@blist, $record); # separate list of browse-only ingest
     push(@$records, $record);
     if (++$count == BATCHSIZE) {
@@ -71,7 +68,7 @@ foreach my $r (@$results) {
 }
 $lol[$lists++] = $records if ($count); # Last batch is likely to be
                                        # small.
-$dbh-&gt;disconnect();
+$dbh->disconnect();
 
 # We're going to reuse $count to keep track of the total number of
 # batches processed.
@@ -84,8 +81,8 @@ my @running = ();
 browse_ingest(@blist);
 
 # We loop until we have processed all of the batches stored in @lol:
-while ($count &lt; $lists) {
-    if (scalar(@lol) &amp;&amp; scalar(@running) &lt; MAXCHILD) {
+while ($count < $lists) {
+    if (scalar(@lol) &amp;&amp; scalar(@running) < MAXCHILD) {
         # Reuse $records for the lulz.
         $records = shift(@lol);
         reingest($records);
@@ -106,19 +103,19 @@ sub reingest {
     my $pid = fork();
     if (!defined($pid)) {
         die "Failed to spawn a child";
-    } elsif ($pid &gt; 0) {
+    } elsif ($pid > 0) {
         push(@running, $pid);
     } elsif ($pid == 0) {
-        my $dbh = DBI-&gt;connect('DBI:Pg:');
-        my $sth = $dbh-&gt;prepare("SELECT metabib.reingest_metabib_field_entries(?, FALSE, TRUE, FALSE)");
+        my $dbh = DBI->connect('DBI:Pg:');
+        my $sth = $dbh->prepare("SELECT metabib.reingest_metabib_field_entries(?, FALSE, TRUE, FALSE)");
         foreach (@$list) {
-            if ($sth-&gt;execute($_)) {
-                my $crap = $sth-&gt;fetchall_arrayref();
+            if ($sth->execute($_)) {
+                my $crap = $sth->fetchall_arrayref();
             } else {
                 die ("Select statement failed for record $_");
             }
         }
-        $dbh-&gt;disconnect();
+        $dbh->disconnect();
         exit(0);
     }
 }
@@ -131,24 +128,23 @@ sub browse_ingest {
     my $pid = fork();
     if (!defined($pid)) {
         die "faild to spawn child";
-    } elsif ($pid &gt; 0) {
+    } elsif ($pid > 0) {
         # Add our browser to the list of running children.
         push(@running, $pid);
         # Increment the number of lists, because this list was not
         # previously counted.
         $lists++;
     } elsif ($pid == 0) {
-        my $dbh = DBI-&gt;connect('DBI:Pg:');
-        my $sth = $dbh-&gt;prepare("SELECT metabib.reingest_metabib_field_entries(?, TRUE, FALSE, TRUE)");
+        my $dbh = DBI->connect('DBI:Pg:');
+        my $sth = $dbh->prepare("SELECT metabib.reingest_metabib_field_entries(?, TRUE, FALSE, TRUE)");
         foreach (@list) {
-            if ($sth-&gt;execute($_)) {
-                my $crap = $sth-&gt;fetchall_arrayref();
+            if ($sth->execute($_)) {
+                my $crap = $sth->fetchall_arrayref();
             } else {
                 die ("Browse ingest failed for record $_");
             }
         }
-        $dbh-&gt;disconnect();
+        $dbh->disconnect();
         exit(0);
     }
 }
-</pre></body></html>
